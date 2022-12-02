0efe4bfc8c3561e1e526e719f3b10919266421ae
khalidumar29 <dreamiwoarld.online@gmail.com>
2022-12-03 03:25:19 +0600

    todos done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,7 +9,7 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q66zrl2.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tcfxjaz.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -18,14 +18,43 @@ const client = new MongoClient(uri, {
 
 const run = async () => {
   try {
-    const db = client.db("moontech");
-    const productCollection = db.collection("product");
+    const db = client.db("moon-tech");
+    const productCollection = db.collection("products");
+    const todoCollection = db.collection("todos");
 
     app.get("/products", async (req, res) => {
       const cursor = productCollection.find({});
       const product = await cursor.toArray();
 
       res.send({ status: true, data: product });
+    });
+
+    app.post("/todo", async (req, res) => {
+      const todo = req.body;
+      const result = await todoCollection.insertOne(todo);
+      res.send({ status: true, data: result });
+    });
+
+    app.patch("/todo/:id", async (req, res) => {
+      const id = req.params.id;
+      const todo = req.body;
+      const result = await todoCollection.updateOne(
+        { _id: ObjectId(id) },
+        { $set: todo }
+      );
+      res.send({ status: true, data: result });
+    });
+
+    app.get("/todo", async (req, res) => {
+      const cursor = todoCollection.find({});
+      const todos = await cursor.toArray();
+      res.send({ status: true, data: todos });
+    });
+
+    app.delete("/todo/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await todoCollection.deleteOne({ _id: ObjectId(id) });
+      res.send({ status: true, data: result });
     });
   } finally {
   }
